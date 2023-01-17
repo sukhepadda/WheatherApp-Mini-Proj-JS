ad6ec3d1d50a197d8e9f141184410b65fdf5b58c
sukhepadda <paddasukhwantsingh@gmail.com>
2023-01-17 12:20:36 +0530

    js is done styling is done but there still some changes need but in future

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('hello world')
+// console.log('hello world')
+
+let showResult = document.querySelector("#showResult");
+let getLocationBtn = document.querySelector("#getLocationBtn");
+
+async function getApiData(latitude, longitude) { 
+    const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=9f0625e04d9241c599a43408231701&q=${latitude}, ${longitude}&aqi=yes`);
+    return await promise.json();
+
+}
+async function gotLocation(position){
+    // window.location.reload();
+    // console.log(position);
+    const finalResult = await getApiData(
+        position.coords.latitude,
+        position.coords.longitude
+    );
+    // console.log(finalResult);
+    showResult.innerHTML = `<h2 class="locationData">You current wheather of ${finalResult.location.name}, ${finalResult.location.region}-${finalResult.location.country} is </h2> <br> <h1 class="tempData">${finalResult.current.temp_c}&#xb0; Celsius and ${finalResult.current.temp_f}&#xb0; Fahrenheit </h1>`
+    
+}
+function failToGetLocation(){
+    showResult.innerHTML = `<h4 class="errorData">There was an error pls allow us permission if you're not.</h4>`;
+}
+
+getLocationBtn.addEventListener('click', async () => {
+    navigator.geolocation.getCurrentPosition(gotLocation, failToGetLocation); //getCurrentPosition get two callback function as an argument first for success and second for errors
+    showResult.innerHTML = `<h2 class="loadingData">Loading...</h4>`;
+
+})
